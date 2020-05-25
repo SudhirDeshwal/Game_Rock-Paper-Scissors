48667e86035066b4af93cc91af8683870547ab92
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-05-25 03:38:31 -0400

    Game is functional now

--- main.js
+++ main.js
@@ -90,6 +90,21 @@ function rpsFrontEnd(humanImagechoice, botImagechoice, finalMessage){
     document.getElementById('rock').remove();
     document.getElementById('paper').remove();
     document.getElementById('scissors').remove();
+   
+    var humanDiv =  document.createElement('div');
+    var botDiv = document.createElement("div");
+    var messageDiv = document.createElement('div');
+
+
+humanDiv.innerHTML="<img src='" + imagesDatabase[humanImagechoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(37, 50, 233, 1);'>"
+botDiv.innerHTML="<img src='" + imagesDatabase[botImagechoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243, 38, 24, 1);'>"
+messageDiv.innerHTML ="<h1 style='color: " + finalMessage['color'] + "; font-size: 60 px ; padding: 30px; '>" + finalMessage['message'] + "</h1>"
+
+document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+document.getElementById('flex-box-rps-div').appendChild(botDiv);
+document.getElementById('flex-box-rps-div').appendChild(messageDiv);
+
+
 }
 
 
